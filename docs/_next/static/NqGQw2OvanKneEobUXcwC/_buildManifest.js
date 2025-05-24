ef17b284f7eb789db086c27d01d633464b7f3974
jaemin25 <ljmjm25@naver.com>
2025-05-24 15:10:29 +0900

    Fix: remove old index.html and re-export

---
+++ docs/_next/static/NqGQw2OvanKneEobUXcwC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/57-510a4d17ba5837b3.js","static/chunks/pages/index-732bf6d49a3b78eb.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
